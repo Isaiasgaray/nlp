dbd96b169b6074b80d6c7371418683f26f222219
Isaías <isaiasgaraygorta@gmail.com>
2023-10-20 23:05:15 -0300

    Corrige ejercicio 1

--- tp1/scraper/scraper.py
+++ tp1/scraper/scraper.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 from datetime import datetime
+import pandas as pd
 import requests
 import locale
 
@@ -8,17 +9,35 @@ LOCALE_AR = 'es_AR.UTF-8'
 locale.setlocale(locale.LC_TIME, LOCALE_AR)
 
 DATE_FORMAT = '%d de %B %Y %H:%Mhs'
-# datetime.strptime('20 de octubre 2023', date_format)
 
-URL = 'https://www.lacapital.com.ar/secciones/ultimo-momento.html'
+# Funciones para procesar los links
+def procesar_titulo(soup):
+    return soup.find('h1', {'class': 'nota-title'}).text.strip()
 
-# TODO
-# r = requests.get(URL)
-# soup = BeautifulSoup(r.text, 'lxml')
+def procesar_categoria(soup):
+    return soup.find('div', {'class': 'breadcrumbs'})\
+               .findChildren()[2].text.strip()
 
-# TODO
-# for link in soup.find_all('a', {'class': 'cover-link'}):
-#     pass
+def procesar_fecha(soup):
+    fecha = soup.find('span', {'class': 'nota-fecha'}).text
+    hora  = soup.find('span', {'class': 'nota-hora'}).text
+
+    return datetime.strptime(fecha + ' ' + hora, DATE_FORMAT).isoformat()
+
+
+def procesar_contenido(soup):
+
+    parrafos = list()
+
+    for tag in soup.find_all('div', {'class': 'article-body'}):
+        parrafos += tag.find_all('p')
+
+    texto = '\n'.join(
+                        parrafo.text.strip() for parrafo in parrafos
+                        if parrafo.text and '>>' not in parrafo.text
+                     )
+
+    return texto
 
 def procesar_link(link):
 
@@ -26,53 +45,34 @@ def procesar_link(link):
     soup = BeautifulSoup(r.text, 'lxml')
 
     try:
-        # Titulo
-        titulo = soup.find('h1', {'class': 'nota-title'}).text.strip()
-
-        # Categoria
-        # categoria_div = soup.find('div', {'class': 'breadcrumbs'})\
-        #                     .find_all('span')
-
-        # if len(categoria_div) != 3:
-        #     categoria = 'Sin categoría'
-        # else:
-        #     categoria = categoria_div[1].text.strip()
-
-        categoria = soup.find('div', {'class': 'breadcrumbs'})\
-                        .findChildren()[2].text.strip()
-
-        # Fecha
-        fecha = soup.find('span', {'class': 'nota-fecha'}).text
-        hora  = soup.find('span', {'class': 'nota-hora'}).text
-
-        fecha_iso = datetime.strptime(fecha + ' ' + hora, DATE_FORMAT).isoformat()
-
-        # Texto
-        parrafos = list()
-
-        for tag in soup.find_all('div', {'class': 'article-body'}):
-            parrafos += tag.find_all('p')
-
-        texto = '\n'.join(
-                            parrafo.text.strip() for parrafo in parrafos
-                            if parrafo.text and '>>' not in parrafo.text
-                         )
+        titulo    = procesar_titulo(soup)
+        texto     = procesar_contenido(soup)
+        categoria = procesar_categoria(soup)
+        fecha     = procesar_fecha(soup)
 
     except Exception as e:
-        print(f'{link} -----> {e}')
+        print(f'{link} => {e}')
         return tuple([None] * 5)
 
-    return titulo, texto, categoria, link, fecha_iso
+    return titulo, texto, categoria, link, fecha
 
+# Funcion principal
 def main():
-
+    URL = 'https://www.lacapital.com.ar/secciones/ultimo-momento.html'
     r = requests.get(URL)
-
     soup = BeautifulSoup(r.text, 'lxml')
 
-    for tag in soup.find_all('a', {'class': 'cover-link'}):
+    columnas = ['titulo', 'texto', 'categoria', 'url', 'fecha']
+    datos    = list()
 
-        pass
+    for tag in soup.find_all('a', {'class': 'cover-link'}):
+        datos.append(procesar_link(tag.get('href')))
+
+    df = pd.DataFrame(datos, columns=columnas)
+    df.drop_duplicates(inplace=True)
+
+    print(df)
+
 
 if __name__ == '__main__':
     main()
